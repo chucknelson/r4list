3ec4aceabe65c985c67da326fbcef7db86ff5126
Chuck Nelson <nelson.charles@gmail.com>
2016-10-19 05:50:03 -0400

    Updating models and corresponding tests - still in progress.

--- app/models/list.rb
+++ app/models/list.rb
@@ -5,6 +5,8 @@ class List < ActiveRecord::Base
     validates :user, presence: true
 	validates :title, presence: true, length: { minimum: 5 }
 
+	
+
 	def completed?
 		self.items_remaining == 0 && self.items.exists? 
 	end
@@ -12,5 +14,11 @@ class List < ActiveRecord::Base
 	def update_items_remaining
     	self.items_remaining = self.items.where(completed: false).count
 	end
+
+	private
+	# enforce updating of this value through public method only
+	def items_remaining=(val)
+      write_attribute :items_remaining, val
+    end
 
 end
--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -8,28 +8,54 @@ class ListTest < ActiveSupport::TestCase
   def teardown
   end
 
-  # Test Validations
+  # Validations
   test "should not save list without user" do
     list = List.new(title: "My List")
     assert_nil list.user, "User exists but should be nil"
-    assert !list.save, "List saved without a user"
+    assert_not list.save, "List saved without a user"
   end
 
   test "should not save list without title" do
     list = List.new(user: users(:user1))
     assert_nil list.title, "Title exists but should be nil"
-    assert !list.save, "List saved without a title"
+    assert_not list.save, "List saved without a title"
   end
 
   test "should not save list without a title of at least 5 characters" do
     list = List.new(user: users(:user1), title: "List")
-    assert !list.save, "List saved with a title less than 5 characters long"
+    assert_not list.save, "List saved with a title less than 5 characters long"
   end
 
   test "should save list meeting validation requirements" do
     list = List.new(user: users(:user1), title: "My List")
-    assert list.save
+    assert list.save, "List did not save successfully"
   end
+
+  # Methods
+  test "should be incomplete if no items exist (i.e., new empty list)" do
+    list = List.new
+    assert_not list.completed?, "List is completed but no items exist"
+  end
+
+  test "should be complete if no items are remaining" do
+    list = lists(:user1_list_with_one_incomplete_item)
+    assert_not list.completed?, "Test list is already complete"
+    list.items.where(completed: false).first.completed = true
+    #list.update_items_remaining
+    #assert_equal 0, list.items_remaining, "Items remaining is not 0"
+    #assert list.completed?, "Items are all complete but list is incomplete"
+  end
+=begin
+  test "should update items remaining with number of incomplete items" do
+    list = lists(:user1_list1)
+    assert list.items.where(completed: false).size > 0, "Test list has no incomplete items"
+    assert_equal list.items.where(completed: false).size, list.items_remaining, "Test list not set up correctly"
+    
+    list.items.where(completed: false).first.completed = true
+    assert list.items.where(completed: false).size == 0, "Items collection did not update correctly"
+    assert_equal list.items.where(completed: false).size, list.items_remaining, "Items remaining not updated correctly"
+  end
+=end
 
   
 

82a4ff68370b50103d1bf199de440c6718b4533e
Chuck Nelson <nelson.charles@gmail.com>
2019-11-29 19:26:18 +0000

    Apply rake rails:update changes for Rails 4.2

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   get 'login', to: 'sessions#new'
   delete 'logout', to: 'sessions#destroy'
-  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
